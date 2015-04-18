c4b326bea28fb561a62d38688dcd24a62caace31
nabijaczleweli <nabijaczleweli@gmail.com>
2015-04-18 17:07:19 +0200

    Replace `RenderAOEIceSpike.progresses` with on-the-fly computing of `progress` and `wasUp`. Disable AOEIceSpike rendering if already down. Closes #6

--- src/main/scala/com/nabijaczleweli/fancymagicks/render/entity/RenderAOEIceSpike.scala
+++ src/main/scala/com/nabijaczleweli/fancymagicks/render/entity/RenderAOEIceSpike.scala
@@ -1,7 +1,7 @@
 package com.nabijaczleweli.fancymagicks.render.entity
 
 import com.nabijaczleweli.fancymagicks.entity.EntityAOEIceSpike
-import com.nabijaczleweli.fancymagicks.util.{IConfigurable, EntityUtil, ResourceLocationFancyMagicks}
+import com.nabijaczleweli.fancymagicks.util.{EntityUtil, IConfigurable, ResourceLocationFancyMagicks}
 import cpw.mods.fml.relauncher.{Side, SideOnly}
 import net.minecraft.client.renderer.entity.Render
 import net.minecraft.entity.Entity
@@ -9,38 +9,34 @@ import net.minecraftforge.client.model.AdvancedModelLoader
 import net.minecraftforge.common.config.Configuration
 import org.lwjgl.opengl.GL11._
 
-import scala.collection.mutable.{LongMap => mLongMap}
-
 @SideOnly(Side.CLIENT)
 object RenderAOEIceSpike extends Render with IConfigurable {
 	var progressStep = .1f
 
 	private lazy val entityTexture = ResourceLocationFancyMagicks("textures/entity/aoe_spike_ice.png")
 	private lazy val model = AdvancedModelLoader loadModel ResourceLocationFancyMagicks("models/entity/aoe_spike_ice.tcn")
-	private lazy val progresses = mLongMap.empty withDefaultValue 0f -> false
 
 	override def doRender(_entity: Entity, x: Double, y: Double, z: Double, f0: Float, partialTickTime: Float) {
 		val entity = _entity.asInstanceOf[EntityAOEIceSpike]
+		val wasUp = entity.ticksExisted > (1 / progressStep)
+		val progress =
+			0F max {
+				if(wasUp)
+					1F - (entity.ticksExisted - (1 / progressStep)) * progressStep
+				else
+					entity.ticksExisted * progressStep
+			} min 1F
 
-		glPushMatrix()
-			glTranslated(x, y, z)
-		  bindEntityTexture(entity)
+		if(progress > 0F) {
+			glPushMatrix()
+				glTranslated(x, y, z)
+				bindEntityTexture(entity)
 
-			glScalef(1, progresses(entity.getEntityId)._1, 1)
-			model.renderAll()
-		glPopMatrix()
-
-		var (progress, wasUp) = progresses(entity.getEntityId)
-		0F max (progress + progressStep * (if(wasUp) -1 else 1)) min 1F match {
-			case 0 if wasUp =>
-				EntityUtil dispatchSimpleKill entity
-			case 1 =>
-				progress = 1
-				wasUp = true
-			case f =>
-				progress = f
-		}
-		progresses(entity.getEntityId) = progress -> wasUp
+				glScalef(1, progress, 1)
+				model.renderAll()
+			glPopMatrix()
+		} else if(wasUp)
+			EntityUtil dispatchSimpleKill entity
 	}
 
 	override def getEntityTexture(entity: Entity) =
